124b90a07bef3bb5d18c284a861d406689ec7653
Andrew Arpin <200398857@student.georgianc.on.ca>
2019-09-16 18:35:47 -0400

    Add project files.

---
+++ Lab 1 - Addition/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_1___Addition.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
